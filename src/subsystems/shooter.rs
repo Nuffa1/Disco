f0b7960554f322aebbbd6eaef98c115f20a23a39
AashnaDhriti <bkumar2022usa@gmail.com>
2024-09-20 17:00:01 -0500

    shooter subsystem done

--- src/subsystems/shooter.rs
+++ src/subsystems/shooter.rs
@@ -5,25 +5,34 @@ use tokio::time::sleep;
 use crate::constants;
 
 pub struct Shooter {
-    
+    front: SRX,
+    back: SRX,
+    pusher: Solenoid,
+    angle: SRX
 }
 
 impl Shooter {
     pub fn new() -> Self {
         Self {
-            
+            front: SRX::new(constants::shooter::FRONT),
+            back: SRX::new(constants::shooter::BACK),
+            angle: SRX::new(constants::shooter::ANGLE),
+            pusher: Solenoid::new(ModuleType::CTRE, constants::shooter::PUSHER),
         }
     }
 
     pub fn set_shooter(&self, speed: f64) {
-        
+        self.front.set(speed);
+        self.back.set(speed);
     }
 
     pub fn set_angle(&self, speed: f64) {
-        
+        self.angle.set(speed);
     }
 
     pub async fn shoot(&self) {
-        
+        self.pusher.set(true);
+        sleep(Duration::from_millis(250)).await;
+        self.pusher.set(false);
     }
 }
